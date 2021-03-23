568b761bbb34f79a838abe9a9c604c6ab4469612
bakzkndd <50497725+bakzkndd@users.noreply.github.com>
2021-03-23 01:57:04 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+import { Plugin } from '@vizality/entities';
+import { patch, unpatch } from '@vizality/patcher';
+import { getModule } from '@vizality/src/core/modules/webpack';
+
+const getWantedHandler = (mod) => mod._orderedActionHandlers.MESSAGE_DELETE.find((x) => x.actionHandler.toString().includes('revealedMessageId'));
+let deleted = [];
+let original;
+let interval;
+let index = 0;
+
+const run = () => {
+  for (let obj of deleted) {
+	styleMessage(obj);
+  }
+};
+
+const styleMessage = async ({ id }) => {
+  let el = document.getElementById(`chat-messages-${id}`);
+  if (!el) return;
+  
+  if (el.classList.contains('gm-deleted-message')) return;
+  
+  el.classList.add('gm-deleted-message');
+  el.style.backgroundColor = 'rgba(240, 71, 71, 0.1)';
+};
+
+export default class NoMessageDelete extends Plugin {
+  async start () {
+	const deleteMessage = await getModule([ 'register' ])
+
+	try {
+		original = getWantedHandler(deleteMessage);
+	  } catch (e) {
+		console.log('No Message Deletion: Setup failed, retrying...');
+		console.log(e)
+		return setTimeout(this.start, 3000);
+	}
+	
+	index = deleteMessage._orderedActionHandlers.MESSAGE_DELETE.indexOf(getWantedHandler(deleteMessage));
+
+	deleteMessage._orderedActionHandlers.MESSAGE_DELETE[index] = {
+      actionHandler: (obj) => {
+      	// console.log(obj);
+
+        if (deleted.find((x) => x.id === obj.id)) { return; }
+      
+        deleted.push(obj);
+
+        styleMessage(obj);
+      },
+
+      storeDidChange: function() { }
+    };
+
+	console.log('No Message Deletion: Ready');
+  }
+
+  stop () {
+	 
+  }
+}
